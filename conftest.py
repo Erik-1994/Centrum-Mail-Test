3fbe4202e57784dc8857d507f3b3f7a07001ff26
Erik <erik.kokoska1@gmail.com>
2020-09-22 18:31:17 +0200

    change

--- conftest.py
+++ conftest.py
@@ -2,26 +2,7 @@ from pytest import fixture
 from selenium import webdriver
 from config import Config
 
-# env settings -----------------------------------------------------------------------------------------
 
-
-def pytest_addoption(parser):
-    parser.addoption("--env", action="store",
-                     help="Environment to run tests against. (dev,qa)")
-
-
-@fixture(scope='session')
-def env(request):
-    return request.config.getoption('--env')
-
-
-@fixture(scope='session')
-def app_config(env):
-    cfg = Config(env)
-    return cfg
-
-
-# mark fixtures -----------------------------------------------------------------------------------------
 @fixture()
 def email_password():
     password = "test_Email"
